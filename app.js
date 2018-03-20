947af67b865abc3921435ae8fe47f6ce493984a9
Damon Freeman <damonefreeman@gmail.com>
2018-03-20 12:58:40 -0500

    Add files all files

---
+++ app.js
@@ -0,0 +1,19 @@
+function yourFeelings() {
+  var mood = prompt("Please enter Happy or Sad",);
+  if(mood === "Happy") {
+    alert("That is wonderful hunnie, I am so happy that you feel " + mood + " today");
+    
+  } else if(mood === "Sad") {
+      alert("Oh no, I am saddened that you feel " + mood + " today. What's going on sweetie");
+    } 
+    else {
+        alert("Why are you feeling " + mood + " today?. Do you want to talk?");
+    }
+  }
+
+    
+  
+
+
+
+
