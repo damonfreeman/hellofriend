be7560f4e2087c5025fffb0dd94efb20f5ccff7f
Damon Freeman <damonefreeman@gmail.com>
2018-03-20 22:25:04 -0500

    fixed the toUpperCase() function

--- app.js
+++ app.js
@@ -1,18 +1,14 @@
 function yourFeelings() {
-  var mood = prompt("Please enter Happy or Sad",);
-  if(mood === "Happy") {
-    alert("That is wonderful hunnie, I am so happy that you feel " + mood + " today");
-    
-  } else if(mood === "Sad") {
-      alert("Oh no, I am saddened that you feel " + mood + " today. What's going on sweetie");
-    } 
-    else {
-        alert("Why are you feeling " + mood + " today?. Do you want to talk?");
-    }
+  var emotion = prompt("Good morning love, how do you feel today",);
+  var convertCase = emotion.toUpperCase();
+  if(convertCase === "HAPPY") {
+    alert("I am so happy that you feel " + emotion + " today my love.");
+  } else if(convertCase === "SAD") {
+    alert("Oh no, I am so sorry that you feel " + emotion + ". Please tell what's wrong and how can I help");
+  } else {
+    alert("When did you start feeling " + emotion + ". Do you want to talk about it");
   }
-
-    
-  
+}
 
 
 
